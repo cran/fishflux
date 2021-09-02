1e413285220934059dc0a1620f7e5631f1f8c1b8
Nina M. D. Schiettekatte <nina.schiettekatte@gmail.com>
2021-09-02 18:50:02 +0000

    version 0.0.1.5

--- inst/doc/intro_to_fishflux.R
+++ inst/doc/intro_to_fishflux.R
@@ -5,50 +5,6 @@ knitr::opts_chunk$set(
   fig.width=8, fig.height=6
 )
 
-## ---- echo = FALSE, results = "asis", message = FALSE-------------------------
-library(knitr)
-
-tex2markdown <- function(texstring) {
-  writeLines(text = texstring,
-             con = myfile <- tempfile(fileext = ".tex"))
-  texfile <- pandoc(input = myfile, format = "html")
-  cat(readLines(texfile), sep = "\n")
-  unlink(c(myfile, texfile))
-}
-
-textable <- "
-\\begin{table}[h!]
-\\centering
-\\caption{. Overview of inputs, including input parameters, to be specified by the user of the model. k indicates c, n or p. VBGC = von Bertalanffy growth curve.}
-\\begin{tabular}{l l l}
-\\hline
-Symbol & Description & Unit\\\\
-\\hline
-$a_\\textrm{k}$  & Element-specific assimilation efficiency & \\_ \\\\
-$l_\\textrm{t}$  & Total length of individual & cm \\\\
-$linf$  & Asymptotic adult length (VBGC) & cm \\\\
-$\\kappa$  & Growth rate parameter (VBGC) & $\\textrm{yr}^{-1}$ \\\\
-$t_0$  & Age at settlement (VBGC) & $\\textrm{yr}$ \\\\
-$lw_a$  & Parameter length-weight relationship & $\\textrm{g cm}^{-1}$ \\\\
-$lw_b$  & Parameter length-weight relationship & \\_\\\\
-$Q_\\textrm{k}$  & Element-specific body content percentage & \\% \\\\
-$f_\\textrm{0}$  & Metabolic normalisation constant independent of body mass & $\\textrm{g C} \\textrm{g}^{-\\alpha} \\textrm{d}^{-1}$ \\\\
-$alpha$  & Mass-scaling exponent &  \\_\\\\
-$theta$  & Activity scope & \\_ \\\\
-$v$  & Environmental temperature & \\textdegree C \\\\
-$h$  & trophic level & \\_ \\\\
-$r$  & Aspect ratio of caudal fin & \\_ \\\\
-$F0nz$  & Mass-specific turnover rate of N & $\\textrm{g N} \\textrm{g}^{-1} \\textrm{d}^{-1}$ \\\\
-$F0pz$  & Mass-specific turnover rate of P & $\\textrm{g P} \\textrm{g}^{-1} \\textrm{d}^{-1}$ \\\\
-$mdw$ & Ratio of dry mass and wet mass of fish & \\_ \\\\
-$D_\\textrm{k}$  & Elemental stoichiometry of diet & \\% \\\\
-\\hline
-\\end{tabular}
-\\end{table}
-"
-
-tex2markdown(textable)
-
 ## ---- message=TRUE, echo=TRUE-------------------------------------------------
 # example
 fishflux::name_errors("Zebrazoma scopas")
